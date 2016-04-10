8a78385dd9a3897bce552eb38423bd742c2eed52
Kellen Schmidt <kellenschmidt31@gmail.com>
2016-04-10 18:28:45 -0500

    Added rapidXML file and barely started document parser

---
+++ SearchEngine/documentparser.cpp
@@ -0,0 +1,25 @@
+#include "documentparser.h"
+#include "indexinterface.h"
+#include "wordref.h"
+#include <iostream>
+
+using namespace std;
+
+DocumentParser::DocumentParser()
+{
+
+}
+
+void DocumentParser::parseFile(int indexType)
+{
+    // Create the type of the index based on the index type
+    if(indexType == 1)
+        IndexInterface *index = new AvlTree<wordRef>;
+    else if(indexType == 2)
+        //Type dependent on Luke
+        //IndexInterface *index = new HashTableIndex;
+    else
+        cerr << "Error: Could not create index, invalid index type\n";
+
+
+}
---
+++ SearchEngine/documentparser.h
@@ -0,0 +1,14 @@
+#ifndef DOCUMENTPARSER_H
+#define DOCUMENTPARSER_H
+
+
+class DocumentParser
+{
+private:
+
+public:
+    DocumentParser();
+
+};
+
+#endif // DOCUMENTPARSER_H
